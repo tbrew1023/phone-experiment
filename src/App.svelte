2bdabb1eea59bf0310f61dfa9295ea0163bf0acd
Trent Brew <tbrew1023@gmail.com>
2020-11-27 19:58:44 -0600

    initial

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let name; //prop
+
+	let rando; //component state <data() { return { rando: null  }>
+
+	/*function setRando() { //methods: {}
+		rando = Math.random();
+	}*/
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style type="text/scss">
+	$color: blue;
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: $color;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
