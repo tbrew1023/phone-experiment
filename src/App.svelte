2a704e74f6e7c3c87485cc8c4a0b61d2d5df8477
TrentonAlexanderBrew <tbrew@genzai.design>
2020-12-03 06:17:57 -0600

    initial

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,63 @@
 <script>
-	export let name; //prop
 
-	import { fireFetch, firePost } from '../firebase';
-
-	//let db = firebase.firestore();
-	let people = []; //component state <data() { return { rando: null  }>
-
-	function fetchTestData() {
-		fireFetch('test-data', (docs) => {
-			docs.forEach((doc) => {
-				//people.push(doc.data());
-				people = [...people, doc.data()];
-			});
-		});
-	}
+function animate() {
+	console.log('animating...');
+}
 	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button on:click={fetchTestData}>Fetch test data</button>
-	<h3>{people}</h3>
-	{#each people as person}
-		<h4>{person.name}</h4>
-	{/each}
+	<div class="page-container">
+	  <div class="phone">
+		<div class="screen"></div>
+	  </div>
+	</div>
 </main>
 
 <style type="text/scss">
-	$color: blue;
 
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+.page-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	width: 100%;
+}
 
-	h1 {
-		color: $color;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+.phone {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  	position: relative;
+	margin: 0 auto;
+	width: 220px;
+	height: 450px;
+    border-radius: 35px;
+    box-shadow: inset 6px -6px 15px rgba(145,160,180,.45), -10px 30px 40px rgba(118,146,180,.18);
+    top: auto;
+    left: -30px;
+    z-index: auto;
+    background: linear-gradient(to bottom left,#f3f8fb,#e5ebf2);
+    opacity: 1;
+    transition: none;
+    pointer-events: auto;
+	transition: transform 0.1s ease-out;
+  	//transform: matrix3d(.830503,.0167194,0,.00110922,.0915284,.835769,0,.0000440454,0,0,1,0,59,90,0,1);
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.screen {
+    width: 200px;
+    height: 354px;
+	border-radius: 12px;
+	background-image: url("../assets/test.gif");
+	background-position: center;
+	background-size: contain;
+	background-repeat: no-repeat;
+    box-shadow: -1px 1px 4px rgba(107,124,147,.5);
+    transition: opacity 1s ease;
+    transform-origin: 0 0;
+    overflow: hidden;
+    will-change: transform;
+	//transform: matrix3d(.5973395145971294,.01568918980316438,0,.00006432768215272425,.02390394939549776,.5315124761457759,0,-.00003167869727711868,0,0,1,0,649,51,0,1);
+}
 </style>
